c5e0407f46c2ca7386efe44b95694fa50e9079b4
akharon77 <tima.ben10@gmail.com>
2022-10-20 00:17:11 +0300

    fix: cpu.cpp moved

--- cpu/cpu.cpp
+++ cpu/cpu.cpp
@@ -1,22 +1,46 @@
+#include <string.h>
 #include "cpu.h"
 
-void ProcCtor(Proc *cpu, SIZE_TYPE size, int fd)
+void ProcCtor(Proc *cpu, SIZE_TYPE mem_size)
 {
-    ASSERT(cpu != NULL);
+    ASSERT(cpu      != NULL);
+    ASSERT(mem_size > 0);
 
-    cpu->size      = size;
-    cpu->instr_ptr = 0;
-    StackCtor(cpu->stk, 64);
-    read(fd, cpu->buf, size);
+    StackCtor(cpu->stk, 16);
+
+    memset(cpu->buf, 0, MAX_PROG_SIZE);
+
+    cpu->buf_size = 0;
+
+    cpu->mem_size = mem_size;
+    cpu->mem      = (VAL_TYPE*) calloc(mem_size, BYTES_VAL);
+
+    memset(cpu->regs, 0, N_REG);
 }
 
 void ProcDtor(Proc *cpu)
 {
     ASSERT(cpu != NULL);
 
-    cpu->size      = -1;
-    cpu->instr_ptr = 0;
+    StackDtor(cpu->stk);
 
-    memset(cpu->buf, -1, MAX_PROG_SIZE);
+    cpu->mem_size = 0;
+    free(cpu->mem);
+
+    cpu->buf_size = 0;
+    memset(cpu->buf, 0, MAX_PROG_SIZE);
+
+    memset(cpu->regs, 0, N_REG);
 }
 
+void ProcLoadFromFile(Proc *cpu, const char *filename)
+{
+    ASSERT(cpu != NULL);
+
+    int fd_inp = open(filename, O_RDONLY, 0);
+    read(fd_inp, (void*) &cpu->buf_size, BYTES_SIZE);
+
+    read(fd_inp, (void*) cpu->buf, cpu->buf_size);
+    
+    close(fd_inp);
+}
