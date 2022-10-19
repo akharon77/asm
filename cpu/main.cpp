a06d1a865b7951bc849e30ba49ad670dc67110bc
akharon77 <tima.ben10@gmail.com>
2022-10-20 00:13:55 +0300

    ref: cpu code

--- cpu/main.cpp
+++ cpu/main.cpp
@@ -6,27 +6,18 @@
 
 int main()
 {
-    int fd_inp = open("output.bin", O_RDONLY, 0);
-    SIZE_TYPE size = 0;
-    read(fd_inp, (void*) &size, BYTES_SIZE);
-
     Proc cpu = {};
     ProcCtor(&cpu);
 
-    ProcLoadFromFile(&cpu, "output.bin");
+#define STK  cpu.stk
+#define BUF  cpu.buf
+#define IP   cpu.instr_ptr
+#define REGS cpu.regs
 
-    // TODO
-    close(fd_inp);
+    ProcLoadFromFile(&cpu, "output.bin");
 
     while (cpu.instr_ptr < size)
     {
-        CMD_TYPE com = 0;
-        read(fd_inp, &com, BYTES_CMD);
-
-#define STK             cpu.stk
-#define BUF             cpu.buf
-#define IP              cpu.instr_ptr
-#define REGS            cpu.regs
 
 #define CMD_DEF(name, arg, code) \
     case CMD_##name:             \
@@ -34,33 +25,32 @@ int main()
         code                     \
         break;          
 
-#define JMP_DEF(name, cond) CMD_DEF(name, LBL_ARG, \
-        {                                          \
-            LBL_TYPE lbl = 0;                      \
-                                                   \
-            INC(CMD_FLAGS);                        \
-                                                   \
-            lbl = ARG(LBL);                        \
-            INC(LBL);                              \
-                                                   \
-            if (cond)                              \
-                IP = lbl;                          \
+#define JMP_DEF(name, cond)   \
+    CMD_DEF(name, LBL_ARG,    \
+        {                     \
+            LBL_TYPE lbl = 0; \
+                              \
+            INC(CMD_FLAGS);   \
+                              \
+            lbl = ARG(LBL);   \
+            INC(LBL);         \
+                              \
+            if (cond)         \
+                IP = lbl;     \
         })
 
-        switch ((CMD_TYPE) (*((CMD_TYPE*) &com) & COM_MASK))
+        switch (ARG(CMD_TYPE) & COM_MASK)
         {
             #include "cmd_def.h"
+        }
+
 #undef CMD_DEF
 #undef JMP_DEF
-        }
+
     }
 
-    StackDtor(&stk);
+    ProcDtor(&proc);
 
     return 0;
 }
 
-int64_t ProcGetValue(Proc *proc)
-{
-    FLAGS_TYPE flags = 
-}
